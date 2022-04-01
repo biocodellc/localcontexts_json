27f48b441db36256f37580e7fd8488caaa2df6a8
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-04-01 15:28:28 -0700

    add lang script

--- scripts/iana.py
+++ scripts/iana.py
@@ -1,17 +1,30 @@
-from urllib import response
 import requests
 import json
 
-response = requests.get('https://raw.githubusercontent.com/OnroerendErfgoed/language-tags/develop/language_tags/data/json/registry.json')
-data = response.json()
+data = requests.get('https://raw.githubusercontent.com/OnroerendErfgoed/language-tags/develop/language_tags/data/json/registry.json').json()
 
-language_list = []
+# OUTPUT: [{'tag': 'ru', 'language': ['Russian']}, {'tag': 'ro', 'language': ['Romanian', 'Moldavian', 'Moldovan']}, ]
+def list():
+    language_list = []
 
-for item in data:
-    if item['Type'] == 'language':
-        obj = {'tag': item['Subtag'], 'language': item['Description']}
-        language_list.append(obj)
+    for item in data:
+        if item['Type'] == 'language':
+            obj = {'tag': item['Subtag'], 'language': item['Description']}
+            language_list.append(obj)
 
-# write list to json file
-with open('../data/iana.json', 'w') as file:
-    json.dump(language_list, file)
+    # write list to json file
+    with open('../data/iana.json', 'w') as file:
+        json.dump(language_list, file)
+
+
+# OUTPUT:  {'Russian':'ru', 'Romanian':'to'}
+def key_value():
+    language_dict = {}
+    for item in data:
+        if item['Type'] == 'language':
+            for lang in item['Description']:
+                language_dict[lang] = item['Subtag']
+
+    # write list to json file
+    with open('../data/ianaDict.json', 'w') as file:
+        json.dump(language_dict, file)
